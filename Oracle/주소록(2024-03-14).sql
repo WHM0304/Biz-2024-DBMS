b9a69d50098f643afc50ba8d2c9da7d2fd15f4b8
WHM0304 <whm0304@naver.com>
2024-03-14 16:41:17 +0900

    oricle

--- Oracle/주소록(2024-03-14).sql
+++ Oracle/주소록(2024-03-14).sql
@@ -4,12 +4,65 @@
 tbl_address 테이블을 생성하시오
 */
 
-CREATE TABLE tbl_address
-(a_code	VARCHAR2(6)		PRIMARY KEY,
-a_name	nVARCHAR2(20)	NOT NULL	,
-a_tel	VARCHAR2(15)	NOT NULL	,
-a_addr	nVARCHAR2(125)		,
-a_hobby1	nVARCHAR2(20)	,	
-a_hobby2	nVARCHAR2(20)	,	
-a_hobby3	nVARCHAR2(20)		
+CREATE TABLE tbl_address (
+    a_code	VARCHAR2(6)		PRIMARY KEY,
+    a_name	nVARCHAR2(20)	NOT NULL	,
+    a_tel	VARCHAR2(15)	NOT NULL	,
+    a_addr	nVARCHAR2(125)		,
+    a_hobby1	nVARCHAR2(20)	,	
+    a_hobby2	nVARCHAR2(20)	,	
+    a_hobby3	nVARCHAR2(20)		
 );
+
+SELECT COUNT(*) FROM tbl_address;
+SELECT * FROM tbl_address;
+
+-- 이름순으로 오름차순 정렬하여 SELECT 하기
+
+SELECT * FROM tbl_address
+ORDER BY a_name ASC;
+
+-- 이름 , 전화번호 , 취미1 칼럼만 나타나도록 Projection 실행
+
+SELECT a_name, a_tel,a_hobby1
+FROM tbl_address;
+
+-- 이름 칼럼에 민 이란 단어가 포함된 리스트만 SELECTION 실행
+
+SELECT * FROM tbl_address
+WHERE a_name LIKE  '%민%'
+ORDER BY a_name;
+
+-- 취미1 의 각 취미별 인원수를 계산하기
+
+SELECT a_hobby1 as 취미 , count(a_hobby1) as 사람수
+FROM tbl_address
+GROUP BY a_hobby1
+ORDER BY 사람수 DESC;
+
+-- 주소록 테이블에는 취미1, 취미2, 취미3 이 있다.
+-- 여행이 취미인 사람 리스트를 출력하기
+
+SELECT *
+FROM tbl_address
+WHERE a_hobby1 = '여행'
+OR a_hobby2 = '여행'
+OR a_hobby3 = '여행'
+ORDER BY a_name;
+
+-- UNION 은 다른것들 다 포함해줌 단 , 칼럼 숫자가 같아야함
+SELECT * FROM
+(
+SELECT a_hobby1 AS 취미 FROM tbl_address WHERE a_hobby1 IS NOT NULL GROUP BY a_hobby1
+UNION
+SELECT a_hobby2 FROM tbl_address WHERE a_hobby2 IS NOT NULL GROUP BY a_hobby2
+UNION
+SELECT a_hobby3 FROM tbl_address WHERE a_hobby3 IS NOT NULL GROUP BY a_hobby3
+)
+ORDER BY 취미 ;
+
+
+
+
+
+
